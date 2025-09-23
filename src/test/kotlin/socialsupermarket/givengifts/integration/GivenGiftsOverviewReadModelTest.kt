32580f1bdddae0b8378450e45ca362336b5ed7b5
remco <remcoruijsenaars@gmail.com>
2025-09-23 21:58:23 +0200

    GivenGiftsOverviewModel test added

--- src/test/kotlin/socialsupermarket/givengifts/integration/GivenGiftsOverviewReadModelTest.kt
+++ src/test/kotlin/socialsupermarket/givengifts/integration/GivenGiftsOverviewReadModelTest.kt
@@ -1,6 +1,7 @@
 package socialsupermarket.givengifts.integration
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.queryhandling.QueryGateway
 import org.junit.jupiter.api.Test
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import socialsupermarket.common.support.BaseIntegrationTest
 import socialsupermarket.common.support.awaitUntilAsserted
 import socialsupermarket.domain.DEFAULT_FUNDING_ID
+import socialsupermarket.domain.commands.contribution.RequestSupportCommand
 import socialsupermarket.domain.commands.contribution.StartContributionYearCommand
 import socialsupermarket.domain.commands.funding.AssessSupportRequestCommand
 import socialsupermarket.domain.commands.funding.RegisterGiftCommand
@@ -16,6 +18,7 @@ import socialsupermarket.fundingbalance.CurrentBalanceReadModel
 import socialsupermarket.givengifts.Gift
 import socialsupermarket.givengifts.GivenGiftsOverviewQuery
 import socialsupermarket.givengifts.GivenGiftsOverviewReadModel
+import socialsupermarket.requestsupport.RequestSupportContributionAggregateTest.Companion.REQUESTED_FOR
 import java.time.LocalDate
 import java.util.UUID
 
@@ -29,6 +32,15 @@ class GivenGiftsOverviewReadModelTest : BaseIntegrationTest() {
     companion object {
         val CONTRIBUTION_ID: UUID = UUID.randomUUID()
         val MEMBER_ID: UUID = UUID.randomUUID()
+        val REQUEST_ID: UUID = UUID.randomUUID()
+        val REQUEST_BY: UUID = UUID.randomUUID()
+        val REQUESTED_FOR: UUID = UUID.randomUUID()
+        val RELATIONSHIP = "Family"
+        val MONTH = "January"
+        val AMOUNT = 80.0
+        val NOTES = "Some extra's"
+        val REQUEST_DATE = LocalDate.now()
+        val REQUESTED_FOR_NAME = "Isa Ruijsenaars"
     }
 
     @Test
@@ -40,7 +52,7 @@ class GivenGiftsOverviewReadModelTest : BaseIntegrationTest() {
             val actualReadModel =
                 queryGateway.query(GivenGiftsOverviewQuery(CONTRIBUTION_ID), GivenGiftsOverviewReadModel::class.java)
 
-            Assertions.assertThat(actualReadModel.get())
+            assertThat(actualReadModel.get())
                 .isEqualTo(
                     GivenGiftsOverviewReadModel().apply {
                         givenTotalAmount = 0.0
@@ -50,6 +62,53 @@ class GivenGiftsOverviewReadModelTest : BaseIntegrationTest() {
                     }
                 )
         }
+    }
+
+
+    @Test
+    fun `1 gift given`() {
+        commandGateway.sendAndWait<Any>(RegisterGiftCommand(DEFAULT_FUNDING_ID, 100.0))
+        commandGateway.sendAndWait<Any>(StartContributionYearCommand(contributionId = CONTRIBUTION_ID, memberId = MEMBER_ID, startDate = LocalDate.now()))
+        commandGateway.sendAndWait<Any>(RequestSupportCommand(
+            contributionId = CONTRIBUTION_ID,
+            requestId = REQUEST_ID,
+            requestedBy = REQUEST_BY,
+            requestedFor = REQUESTED_FOR,
+            relationShip = RELATIONSHIP,
+            month = MONTH,
+            amount = AMOUNT,
+            notes = NOTES,
+            requestDate = REQUEST_DATE,
+            requestedForName = REQUESTED_FOR_NAME
+        )
+        )
+
+        awaitUntilAsserted {
+            val actualReadModel =
+                queryGateway.query(GivenGiftsOverviewQuery(CONTRIBUTION_ID), GivenGiftsOverviewReadModel::class.java)
+
+            assertThat(actualReadModel.get())
+                .isEqualTo(
+                    GivenGiftsOverviewReadModel().apply {
+                        givenTotalAmount = AMOUNT
+                        givenApprovedAmount = AMOUNT
+                        givenRequestedAmount = 0.0
+                        givenGifts = mutableListOf(Gift(
+                            requestId = REQUEST_ID,
+                            date = REQUEST_DATE,
+                            requestedForName = REQUESTED_FOR_NAME,
+                            reason = NOTES,
+                            amount = AMOUNT,
+                            status = "APPROVED"
+                        ))
+                    }
+                )
+        }
+    }
+
+    @Test
+    fun `1 gift given and 1 gift requested`() {
+        //TODO
     }
 
 }
